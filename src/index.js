778bea6384adf512196f9f841fa7cec17b098a0e
dazzlingeffe <irusya9@mail.ru>
2020-09-29 13:49:00 +0300

    feat: functions min, max, avg done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!arguments.length || !array.length) {
+    return 0;
+  }
+  let minimum = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < minimum) {
+      minimum = array[i];
+    }
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!arguments.length || !array.length) {
+    return 0;
+  }
+  let maximum = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maximum) {
+      maximum = array[i];
+    }
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!arguments.length || !array.length) {
+    return 0;
+  }
+  let summ = 0;
+  for (let i = 0; i < array.length; i++) {
+    summ += array[i];
+  }
+  return summ / array.length;
 }
